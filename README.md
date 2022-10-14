255e92601e7a8ea032e21e1323224ca8805068e4
Shravan <69118069+Shravan-1908@users.noreply.github.com>
2022-10-14 09:04:07 +0000

    added config.json file

--- README.md
+++ README.md
@@ -2,6 +2,18 @@
 
 The program reminds you to drink water, do some physical exercise and make some eye movement after regular intervals of time by playing respective mp3 files. It also logs the timestamp of the activity (like drinking water, physical exercise and eye movement) performed in a `log_timestamp_file.txt` file.
 
-You can run this program by downloading `eye.mp3`, `exercise.mp3` , `water.mp3` in the same directory as the `healthyify_me.py` file
+The default timeouts for eye, water and exercise breaks are 1200s, 3600s and 4800s respectively.
+
+You can change them by adding a config.json file in the same directory as of the python file, like this:
+
+```json
+{
+	"eyes_timeout": 1200,
+	"water_timeout": 3600,
+	"exercise_timeout": 2400
+}
+```
+
+You can run this program by downloading `eye.mp3`, `exercise.mp3` , `water.mp3` in the same directory as the `healthy_me.py` file.
 
 This program makes you healthy by incorporating a routine of regular eye and body movements and keeping you hydrated!
--- healthy_me.py
+++ healthy_me.py
@@ -1,5 +1,38 @@
 import time
+import json
+from dataclasses import dataclass
 from pygame import mixer
+
+
+@dataclass
+class Config:
+    """
+    Configuration dataclass. Holds timeouts for different breaks.
+    """
+
+    eyes_timeout: int
+    water_timeout: int
+    exercise_timeout: int
+
+    def __init__(self) -> None:
+        self.__read()
+
+    def __read(self):
+        """
+        Read configuration from config.json, if present.
+        """
+        try:
+            with open("./config.json") as f:
+                data = json.load(f)
+                self.eyes_timeout = data["eyes_timeout"]
+                self.water_timeout = data["water_timeout"]
+                self.exercise_timeout = data["exercise_timeout"]
+
+        except Exception:
+            self.eyes_timeout = 1200
+            self.water_timeout = 3600
+            self.exercise_timeout = 4800
+
 
 def play_music(stop_signal: str, song: str) -> None:
     """
@@ -11,23 +44,27 @@ def play_music(stop_signal: str, song: str) -> None:
 
     Plays `song` after desired time interval and stops the song when the `stop_signal` is entered in the terminal
     """
-    #starting the mixer
-    mixer.init()
+    try:
+        # starting the mixer
+        mixer.init()
 
-    #loading the music
-    mixer.music.load(song)
+        # loading the music
+        mixer.music.load(song)
 
-    print(f"Playing {song}...")
-    #start playing the music
-    mixer.music.play()
+        print(f"Playing {song}...")
+        # start playing the music
+        mixer.music.play()
 
-    #taking input from the user to enter the `stop_signal`
-    while True:
-        entry = input(f"Enter '{stop_signal}' to stop the song: ")
-        if entry.lower() == stop_signal:
-            #stopping the music
-            mixer.music.stop()
-            break
+        # taking input from the user to enter the `stop_signal`
+        while True:
+            entry = input(f"Enter '{stop_signal}' to stop the song: ")
+            if entry.lower() == stop_signal:
+                # stopping the music
+                mixer.music.stop()
+                break
+
+    except Exception as e:
+        print(f"Unable to play audio! An error occured: \n{e}.")
 
 
 def file_writing(activity: str) -> None:
@@ -53,39 +90,35 @@ def file_writing(activity: str) -> None:
 
 if __name__ == "__main__":
 
-    #initialising time
+    # initialising time
     init_eyes = time.time()
     init_water = time.time()
     init_exercise = time.time()
 
-    #setting up the time intervals for eye movement, drinking water and physical exercise
-    eyes_interval = 1200
-    water_interval = 3600
-    exercise_interval = 2400
+    # setting up the time intervals for eye movement, drinking water and physical exercise
+    config = Config()
 
-    while(True):
+    while True:
         # for eyes movement
-        if ((time.time() - init_eyes) >= eyes_interval):
+        if (time.time() - init_eyes) >= config.eyes_timeout:
             print("\nIts your eyes movement time!")
             play_music("eydone", "eye.mp3")
             init_eyes = time.time()
             file_writing("eye")
-        
-        #for drinking water
-        if ((time.time() - init_water) >= water_interval):
+
+        # for drinking water
+        if (time.time() - init_water) >= config.water_timeout:
             print("\nIts time to drink water!")
             play_music("drank", "water.mp3")
             init_water = time.time()
             file_writing("water")
 
         # for physical activity
-        if ((time.time() - init_exercise) >= exercise_interval):
+        if (time.time() - init_exercise) >= config.exercise_timeout:
             print("\nLets do some physical exercise!")
             play_music("exdone", "exercise.mp3")
             init_exercise = time.time()
             file_writing("exercise")
 
-        #program checks the above mentioned conditions after every 2 secs
+        # program checks the above mentioned conditions after every 2 secs
         time.sleep(2)
-
-        
